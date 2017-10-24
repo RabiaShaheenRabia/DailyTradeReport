920932f3d9d021a289d07ead73bef2cf47fb1df2
Rabia <shaheen.rabia@gmail.com>
2017-10-24 12:28:23 +0100

    DailyTradeReport : ADD boolean exited for testing main method.

--- src/test/java/com/jpm/trade/TestSuite.java
+++ src/test/java/com/jpm/trade/TestSuite.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         DailyTradingProcessTest.class, //test case 1
-        //ReportGeneratorTest.class     //test case 2
+        MainTest .class   //test case 2
 })
 public class TestSuite {
 }
