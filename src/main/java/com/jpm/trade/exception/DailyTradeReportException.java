cddde7fd39d4480c503b0d74cb303d2c0581b70e
Rabia <shaheen.rabia@gmail.com>
2017-10-25 18:46:02 +0100

    DailyTradeReport : ADDED Exception class

---
+++ src/main/java/com/jpm/trade/exception/DailyTradeReportException.java
@@ -0,0 +1,11 @@
+package com.jpm.trade.exception;
+
+/**
+ * Created by Rabia on 25/10/2017.
+ */
+public class DailyTradeReportException extends  Exception{
+
+    public DailyTradeReportException(String msg){
+        super(msg);
+    }
+}
