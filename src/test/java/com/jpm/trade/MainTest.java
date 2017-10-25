e023a2ec2f8aa090d1cbbf15da144dba9307cc63
Rabia <shaheen.rabia@gmail.com>
2017-10-25 19:07:27 +0100

    DailyTradeReport : ADDED Test ces for wrong file format

--- src/test/java/com/jpm/trade/MainTest.java
+++ src/test/java/com/jpm/trade/MainTest.java
@@ -27,4 +27,16 @@ public class MainTest{
         Main.main(args);
     }
 
+    @Test
+    public void testMainWrongFileFormat() {
+        System.out.println("main");
+        String[] args ={"",""};
+        String fileName = "test-trade-input1.txt";
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path= classLoader.getResource(fileName).getPath().replace("/C:/","C:/");
+        args[0] = path;
+        exit.expectSystemExitWithStatus(-1);
+        Main.main(args);
+    }
+
 }
